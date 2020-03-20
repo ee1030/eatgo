ecee859e7628bc87f638ea97de6c1ee7fd314cc4
ee1030 <sksguswoek1@gmail.com>
2020-03-20 19:25:05 +0900

    added Restaurants List

--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/EatgoApplication.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/EatgoApplication.java
@@ -2,8 +2,10 @@ package kr.co.fastcampus.eatgo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"kr.co.fastcampus.eatgo.*"})
 public class EatgoApplication {
 
 	public static void main(String[] args) {
--- eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
+++ eatgo-api/src/main/java/kr/co/fastcampus/eatgo/interfaces/RestaurantController.java
@@ -1,29 +1,35 @@
 package kr.co.fastcampus.eatgo.interfaces;
 
+import kr.co.fastcampus.eatgo.application.RestaurantService;
+import kr.co.fastcampus.eatgo.domain.MenuItem;
+import kr.co.fastcampus.eatgo.domain.MenuItemRepository;
 import kr.co.fastcampus.eatgo.domain.Restaurant;
+import kr.co.fastcampus.eatgo.domain.RestaurantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
 import java.util.List;
+
 
 @RestController
 public class RestaurantController {
 
+    @Autowired
+    private RestaurantService restaurantService;
+
     @GetMapping("/restaurants")
     public List<Restaurant> list() {
-        List<Restaurant> restaurants = new ArrayList<>();
+        List<Restaurant> restaurants = restaurantService.getRestaurants();
 
-        Restaurant restaurant = new Restaurant(1004L,"Bob zip", "Seoul");
-
-        restaurants.add(restaurant);
         return restaurants;
     }
 
     @GetMapping("/restaurants/{id}")
     public Restaurant detail(@PathVariable("id") Long id) {
-        Restaurant restaurant = new Restaurant(1004L,"Bob zip", "Seoul");
+        Restaurant restaurant = restaurantService.getRestaurant(id);
+        // 기본정보 + 메뉴 정보
+
         return restaurant;
     }
 }
