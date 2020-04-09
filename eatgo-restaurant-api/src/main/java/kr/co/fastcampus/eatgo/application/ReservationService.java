55959672e881c144256e55446b368ed798850c23
ee1030 <sksguswoek1@gmail.com>
2020-04-09 18:26:24 +0900

    added Table Reservation - 3

--- eatgo-restaurant-api/src/main/java/kr/co/fastcampus/eatgo/application/ReservationService.java
+++ eatgo-restaurant-api/src/main/java/kr/co/fastcampus/eatgo/application/ReservationService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @Transactional
@@ -17,20 +18,7 @@ public class ReservationService {
     public ReservationService(ReservationRepository reservationRepository) {
         this.reservationRepository = reservationRepository;
     }
-
-    public Reservation addReservation(Long restaurantId, Long userId, String name,
-                                      String date, String time, Integer partySize) {
-        Reservation reservation = Reservation.builder()
-                .restaurantId(restaurantId)
-                .userId(userId)
-                .name(name)
-                .date(date)
-                .time(time)
-                .partySize(partySize)
-                .build();
-
-        return reservationRepository.save(reservation);
-
+    public List<Reservation> getReservations(long restaurantId) {
+        return reservationRepository.findAllByRestaurantId(restaurantId);
     }
-    //
 }
